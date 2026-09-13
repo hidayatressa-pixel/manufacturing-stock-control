5f47d80be4b2f6292c600a434d70d1efb74ac2e0
Ressa Hidayat <hidayatressa@gmail.com>
2026-09-13 09:24:24 +0700

    mount excel and report tools

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App';
+import DataTools from './DataTools';
 import './style.css';
-createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>);
+createRoot(document.getElementById('root')!).render(<React.StrictMode><App/><DataTools/></React.StrictMode>);
